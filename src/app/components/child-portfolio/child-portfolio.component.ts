64daa5d6631f251d242b1411c33b14c4004f761d
Mousa <mousasalib601@gmail.com>
2023-10-26 23:54:28 +0300

    Uploading projects

--- src/app/components/child-portfolio/child-portfolio.component.ts
+++ src/app/components/child-portfolio/child-portfolio.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./child-portfolio.component.css']
 })
 export class ChildPortfolioComponent {
+  images: any[] = [
+    {
+      imagePath: "assets/Media/adminstrationOfproducts.png",
+      typeOfProject: "Html, Css, Javascript",
+      description: "Visit the project by clicking on the button Visit.",
+      link: "https://mousasalib.github.io/administrationOfProducts/"
+    },
+    {
+      imagePath: "assets/Media/toDoApp.png",
+      typeOfProject: "Angular",
+      description: "Visit the project by clicking on the button Visit.",
+      link: "https://to-do-app-omega-sandy.vercel.app/"
+    },
+    {
+      imagePath: "assets/Media/users.png",
+      typeOfProject: "Angular",
+      description: "Visit the project by clicking on the button Visit. ",
+      link: "https://users-app-amber.vercel.app/"
+    }
+  ];
+
+
 
 }
